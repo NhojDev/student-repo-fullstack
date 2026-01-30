c1c1417c7787088bfcfeb48616d5a81f8b49fc12
NhojDev <johnduypham@gmail.com>
2026-01-30 10:33:52 -0800

    Updated to Math.floor instead of having a loop

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -6,33 +6,26 @@ function calculateChange(amount) {
     let cents = Math.round(amount * 100)
     if (cents > 10000) 
         return "Error: the number is too large";
-    let dollars = 0;
-    let quarters = 0;
-    let dimes = 0;
-    let nickels = 0;
-    let pennies = 0;
-    while (cents > 0) {
-        if (cents >= 100) {
-            cents -= 100;
-            dollars += 1;
-        }
-        else if (cents >= 25) {
-            cents -= 25;
-            quarters += 1;
-        }
-        else if (cents >= 10) {
-            cents -= 10;
-            dimes += 1;
-        }
-        else if (cents >= 5) {
-            cents -= 5;
-            nickels += 1;
-        }
-        else if (cents >= 1) {
-            cents -= 1;
-            pennies += 1;
-        }
+    if (cents < 0) 
+        return "Error: the number is negative";
+    if (cents < 0 || isNaN(cents)) {
+        return "Error: invalid amount";
     }
+
+    const dollars = Math.floor(cents / 100);
+    cents %= 100;
+
+    const quarters = Math.floor(cents / 25);
+    cents %= 25;
+
+    const dimes = Math.floor(cents / 10);
+    cents %= 10;
+
+    const nickels = Math.floor(cents / 5);
+    cents %= 5;
+
+    const pennies = cents;
+
     return `${dollars} dollars, ${quarters} quarters, ${dimes} dimes, ${nickels} nickels, ${pennies} pennies`;
 }
 
@@ -50,3 +43,5 @@ console.log(calculateChange(12.34));
 console.log(calculateChange(5.67));
 console.log(calculateChange(1.41));
 console.log(calculateChange(0.99));
+console.log(calculateChange(-34.99));
+console.log(calculateChange("hello"));
